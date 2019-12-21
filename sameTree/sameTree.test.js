7067fb3cadc1c4231c1d7feeecce589bdf26f774
Aditya Vyas <vyasaditya41@yahoo.in>
2019-12-22 03:14:31 +0530

    Solve sameTree LC

---
+++ sameTree/sameTree.test.js
@@ -0,0 +1,15 @@
+const isSameTree = require('./sameTree');
+
+describe('The Trees should be same',() => {
+    test('should return true if both are same', () => {
+        expect((true)).toBe(true)
+    });
+
+    test('should return true if both are same', () => {
+        expect((false)).toBe(false)
+    });
+
+    test('should return true if both are same', () => {
+        expect((false)).toBe(false)
+    });
+});
